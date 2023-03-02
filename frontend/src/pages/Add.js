9d8727947eae221f1249144b1f373ff7112cc4f4
daisuke.miyoshi <nana40481023@gmail.com>
2023-03-03 03:08:24 +0900

    axiosのテスト

--- frontend/src/pages/Add.js
+++ frontend/src/pages/Add.js
@@ -1,5 +1,31 @@
+import axios from 'axios';
+import { useState } from 'react';
+
 export default function Add() {
+  const [image,setImage] = useState();
+
+  function postData() {
+    var formData = new FormData();
+    formData.append("lat", 2.23);
+    formData.append("lng", 2.532);
+    formData.append("image", image[0])
+    formData.append("comment", "test")
+
+
+    axios
+      .post("http://localhost:8000/garbage/garbage/", formData)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((res) => {
+        console.log(res);
+      })
+  }
+
   return (
-    <input type="file" accept="image/png, image/jpeg"></input>
+    <>
+      <input type="file" name="file" accept="image/png, image/jpeg" onChange={event => setImage(event.target.files)} />
+      <input type="button" value="test" onClick={postData} />
+    </>
   )
 }
