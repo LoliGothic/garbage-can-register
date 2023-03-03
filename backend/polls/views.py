154740e7d97462a1fab4c4c8275e34209e063609
dorapon48 <dorapon48@icloud.com>
2023-03-03 09:03:57 +0900

    add: Starsのviewsを設定

--- backend/polls/views.py
+++ backend/polls/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render
 from rest_framework import generics, permissions, viewsets, status
 
 # 作成したserializerをインポート
-from .serializers import GarbageSerializer
+from .serializers import GarbageSerializer, StarsSerializer
 # 作成したモデルもインポート
-from .models import Garbage
+from .models import Garbage, Stars
  
 class GarbageViewSet(viewsets.ModelViewSet):
     queryset = Garbage.objects.all()
     serializer_class = GarbageSerializer
+
+class StarsViewSet(viewsets.ModelViewSet):
+    queryset = Stars.objects.all()
+    serializer_class = StarsSerializer
